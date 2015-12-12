1e55beacc71502a1f80bdb207abae27567b163a9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-12-12 22:43:58 +0900

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# parse-user-repo
+
+[![NPM version](https://img.shields.io/npm/v/parse-user-repo.svg)](https://www.npmjs.com/package/parse-user-repo)
+[![Bower version](https://img.shields.io/bower/v/parse-user-repo.svg)](https://github.com/shinnn/parse-user-repo/releases)
+[![Build Status](https://travis-ci.org/shinnn/parse-user-repo.svg)](https://travis-ci.org/shinnn/parse-user-repo)
+[![Coverage Status](https://img.shields.io/coveralls/shinnn/parse-user-repo.svg)](https://coveralls.io/github/shinnn/parse-user-repo?branch=master)
+[![devDependency Status](https://david-dm.org/shinnn/parse-user-repo/dev-status.svg)](https://david-dm.org/shinnn/parse-user-repo#info=devDependencies)
+
+Parse a `username/repo` string
+
+## Installation
+
+### Package managers
+
+#### [npm](https://www.npmjs.com/)
+
+```
+npm install parse-user-repo
+```
+
+#### [Bower](http://bower.io/)
+
+```
+bower install parse-user-repo
+```
+
+### Standalone
+
+[Download the script file directly.](https://raw.githubusercontent.com/shinnn/parse-user-repo/master/browser.js)
+
+## API
+
+### parseUserRepo(*str*)
+
+*str*: `String` (`user/repo` style repository identifier, for example <https://github.com/holman/spark> → `'holman/spark'`)  
+Return: `Object` (`{username: ..., repo: ...}`)
+
+It extracts a username and a repository name from a `user/repo` string and creates an object.
+
+```javascript
+parseUserRepo('spf13/hugo');
+//=> {username: 'spf13', repo: 'hugo'}
+```
+
+## License
+
+Copyright (c) 2014 - 2015 [Shinnosuke Watanabe](https://github.com/shinnn)
+
+Licensed under [the MIT License](./LICENSE).
---
+++ test.js
@@ -0,0 +1,73 @@
+'use strong';
+
+const test = require('tape');
+
+function runTest(description, main) {
+  test(description, t => {
+    t.plan(11);
+
+    t.strictEqual(main.name, 'parseUserRepo', 'should have a function name.');
+
+    t.deepEqual(main('a/b'), {username: 'a', repo: 'b'}, 'should parse a user/repo string.');
+
+    t.throws(
+      () => main(),
+      /^TypeError.*undefined is not a string\. Expected a string in the form of "username\/repo"\./,
+      'should throw a type error when it takes no arguments.'
+    );
+
+    t.throws(
+      () => main(1),
+      /^TypeError.*1 is not a string\. .*e\.g\. isaacs\/minimatch/,
+      'should throw a type error when it takes a non-string argument.'
+    );
+
+    t.throws(
+      () => main(''),
+      /^Error.*Expected a string in the form of "username\/repo", but received an empty string\./,
+      'should throw an error when it takes an empty string.'
+    );
+
+    t.throws(
+      () => main('ab'),
+      /^Error.*ab doesn't include `\/`\. /,
+      'should throw an error when the string does not include `/`.'
+    );
+
+    t.throws(
+      () => main('a//b'),
+      /^Error.*a\/\/b includes more than one slash\. /,
+      'should throw an error when the string includes multiple `/`.'
+    );
+
+    t.throws(
+      () => main('a/ b'),
+      /^Error.*a\/ b includes a white space\. /,
+      'should throw an error when the string includes a white space.'
+    );
+
+    t.throws(
+      () => main('/'),
+      /^Error.*The string includes neither username nor repository name\. /,
+      'should throw an error when it takes `/`.'
+    );
+
+    t.throws(
+      () => main('/b'),
+      /^Error.*\/b starts with `\/`, in other words no username is specified\. /,
+      'should throw an error when the string starts with `/`.'
+    );
+
+    t.throws(
+      () => main('a/'),
+      /Error.*a\/ ends with `\/`, in other words no repository name is specified\. /,
+      'should throw an error when the string ends with `/`.'
+    );
+  });
+}
+
+global.window = {};
+require('./' + require('./bower.json').main);
+
+runTest('require(\'parse-user-repo\')', require('.'));
+runTest('window.parseUserRepo', global.window.parseUserRepo);
